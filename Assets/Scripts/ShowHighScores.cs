968c5d0b550f46a9381c5647a5417a426a42272d
tsarachan <test@notanemailaddress.com>
2016-06-27 20:53:27 -0400

    High score scene
    Created scene for displaying high score list.

--- Assets/Scripts/ShowHighScores.cs
+++ Assets/Scripts/ShowHighScores.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ShowHighScores : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private const string HIGH_SCORE_PATH = "/Scores/";
+	public string highScoreNamesFile = "HighScoreNames.txt";
+
+	List<string> GetNames()
+	{
+		string[] names = FileIO.SplitStringArrayFromFile(Application.dataPath + HIGH_SCORE_PATH + highScoreNamesFile, ',');
+
+		List<string> oldNames = new List<string>();
+
+		foreach (string name in names) { oldNames.Add(name); }
+
+		return oldNames;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void WriteNameList(List<string> nameList)
+	{
+		string names = "";
+
+		for (int i = 0; i < ValueHolder.scoresTracked; i++) { names += name + ","; }
+
+		names = names.Trim(',');
+
+		FileIO.WriteStringToFile(Application.dataPath + HIGH_SCORE_PATH + highScoreNamesFile,
+			names,
+			false);
 	}
 }
