909bb4eb9356e31cb959a875e8a24d7e77755c1e
Prisyafandiafif Charifa <prisyafandiafif.charifa@gmail.com>
2016-06-28 01:33:37 -0400

    + adding rotation animation, compatible with player's dimension switching but not with the control and enemiesGroup + adding hotween for animation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour 
+{
+	public static GameManager instance;
+	
+	[HideInInspector]
+	public GameObject enemiesGroup;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		instance = this;
+
+		enemiesGroup = GameObject.Find("Enemies");
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
 	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Holoville.HOTween;
+using Holoville.HOTween.Plugins; 
 
 public class Player : MonoBehaviour 
 {
@@ -8,6 +10,8 @@ public class Player : MonoBehaviour
 
 	private BoxCollider collider;
 	private bool xyMode;
+	//To know whether the switching is still on process or not
+	private bool isSwitchingHappening; 
 
 	// Use this for initialization
 	void Start () 
@@ -18,22 +22,49 @@ public class Player : MonoBehaviour
 		collider.size = new Vector3(1, 1, 100);
 	}
 	
+	void OnPlayerRotationFinished ()
+	{
+		if (xyMode)
+		{
+			collider.size = new Vector3(1, 100, 1);
+		}
+		else
+		{
+			collider.size = new Vector3(1, 1, 100);
+		}
+
+		xyMode = !xyMode;
+
+		isSwitchingHappening = false;
+	}
+	
+	void OnEnemiesRotationFinished ()
+	{
+
+	}
+	
 	// Update is called once per frame
 	void Update () 
 	{
 		//change dimension
-		if(Input.GetKeyDown(KeyCode.Space)) 
+		if(Input.GetKeyDown(KeyCode.Space) && !isSwitchingHappening) 
 		{
 			if(xyMode) 
 			{
-				collider.size = new Vector3(1, 100, 1);
+				isSwitchingHappening = true;
+
+				//rotate the player and enemies to x rotation to 0
+				HOTween.To(this.gameObject.transform, 0.5f, new TweenParms().Prop("localRotation", new Vector3(-90f,0f,0f), false).OnComplete(OnPlayerRotationFinished).Ease(EaseType.Linear).Delay(0f));
+				HOTween.To(GameManager.instance.enemiesGroup.transform, 0.5f, new TweenParms().Prop("localRotation", new Vector3(-90f,0f,0f), false).OnComplete(OnEnemiesRotationFinished).Ease(EaseType.Linear).Delay(0f));
 			}
 			else 
 			{
-				collider.size = new Vector3(1, 1, 100);
-			}
+				isSwitchingHappening = true;
 
-			xyMode = !xyMode;
+				//rotate the player and enemies to x rotation to 0
+				HOTween.To(this.gameObject.transform, 0.5f, new TweenParms().Prop("localRotation", new Vector3(0f,0f,0f), false).OnComplete(OnPlayerRotationFinished).Ease(EaseType.Linear).Delay(0f));
+				HOTween.To(GameManager.instance.enemiesGroup.transform, 0.5f, new TweenParms().Prop("localRotation", new Vector3(0f,0f,0f), false).OnComplete(OnEnemiesRotationFinished).Ease(EaseType.Linear).Delay(0f));
+			}
 		}
 
 		if (Input.GetKey(KeyCode.W))
