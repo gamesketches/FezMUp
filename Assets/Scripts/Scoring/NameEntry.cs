ee8f3dbed470f30850dcc590104bcd6ffc49a0ef
tsarachan <test@notanemailaddress.com>
2016-06-27 20:14:29 -0400

    Name entry scene
    Added a scene where the player enters a name.

--- Assets/Scripts/Scoring/NameEntry.cs
+++ Assets/Scripts/Scoring/NameEntry.cs
@@ -6,9 +6,6 @@ using System.Collections.Generic;
 
 public class NameEntry : MonoBehaviour {
 
-	private const string HIGH_SCORE_PATH = "/Scores/";
-	public string highScoreNamesFile = "HighScoreNames.txt";
-
 	InputField nameField;
 
 	void Start()
@@ -21,41 +18,8 @@ public class NameEntry : MonoBehaviour {
 	{
 		if (Input.GetKeyDown(KeyCode.Return))
 		{
-			UpdateNameList(nameField.text);
+			ValueHolder.name = nameField.text;
+			SceneManager.LoadScene("main");
 		}
-	}
-
-	void UpdateNameList(string newName)
-	{
-		List<string> nameList = GetNames();
-
-		nameList.Insert(ValueHolder.insertPoint, newName);
-
-		WriteNameList(nameList);
-	}
-
-	List<string> GetNames()
-	{
-		string[] names = FileIO.SplitStringArrayFromFile(Application.dataPath + HIGH_SCORE_PATH + highScoreNamesFile, ',');
-
-		List<string> oldNames = new List<string>();
-
-		foreach (string name in names) { oldNames.Add(name); }
-
-		return oldNames;
-	}
-
-	void WriteNameList(List<string> nameList)
-	{
-		string names = "";
-
-		for (int i = 0; i < ValueHolder.scoresTracked; i++) { names += name + ","; }
-
-		names = names.Trim(',');
-
-		FileIO.WriteStringToFile(Application.dataPath + HIGH_SCORE_PATH + highScoreNamesFile,
-								 names,
-								 false);
-		SceneManager.LoadScene("High scores");
 	}
 }
--- Assets/Scripts/Utilities/ValueHolder.cs
+++ Assets/Scripts/Utilities/ValueHolder.cs
@@ -5,4 +5,5 @@ public class ValueHolder {
 
 	public static int scoresTracked = 10;
 	public static int insertPoint = 0;
+	public static string name = "";
 }
